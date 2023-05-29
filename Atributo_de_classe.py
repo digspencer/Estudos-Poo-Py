8b74354653f5fa47c4ccdf65311af0d1e5e3bcd8
digspencer <diegoiglan@hotmail.com>
2023-05-29 11:33:27 -0300

    Add mais uma classe

---
+++ Atributo_de_classe.py
@@ -0,0 +1,16 @@
+#Atributo de Classe
+
+class Pessoa:
+    ano_atual = 2023
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
+    
+    def get_ano_nascimento(self):
+        return Pessoa.ano_atual - self.idade
+    
+p1 = Pessoa('João', 33)
+p2 = Pessoa('Helena', 11)
+
+print(p1.get_ano_nascimento())
+print(p2.get_ano_nascimento())
